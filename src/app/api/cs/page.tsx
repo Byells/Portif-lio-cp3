1ee57561262b3d4b9a60dad48ad96972c8e16444
Gustavo <gustavo.borba.27@gmail.com>
2024-10-22 23:41:20 -0300

    arrumando detalhes

---
+++ src/app/api/cs/page.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+export default function cs() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/app/api/gs/cad-gs/page.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+// Para cadastro
+export default function gsCad() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
