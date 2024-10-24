a8f25d9aae7b0f6b7954b7dd5e91f4a75a42feac
CauaMachad0 <caua.marcelo.machado@outlook.com>
2024-10-23 21:54:19 -0300

    Atualizando o route.ts do [id]

--- src/app/api/base-route/[id]/route.ts
+++ src/app/api/base-route/[id]/route.ts
@@ -1,45 +1,37 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
-import { Avaliacao } from "@/types";
+import { Avaliacao } from "@/types/types";
 
-
-// Função GET - Obter avaliação por ID
 export async function GET(request: Request, { params }: { params: { id: number } }) {
-  const file = await fs.readFile(process.cwd() + '/src/data/evaluations.json', 'utf-8');
-  const evaluations: Avaliacao[] = JSON.parse(file);
-  const evaluation = evaluations.find(e => e.id == params.id);
+    const file = await fs.readFile(process.cwd() + '/src/data/evaluations.json', 'utf-8');
+    const evaluations: Avaliacao[] = JSON.parse(file);
+    const evaluation = evaluations.find(e => e.id === params.id);
 
-  return evaluation ? NextResponse.json(evaluation) : NextResponse.json({ msg: "Avaliação não encontrada" });
+    return NextResponse.json(evaluation);
 }
 
-// Função PUT - Atualizar avaliação por ID
 export async function PUT(request: Request, { params }: { params: { id: number } }) {
-  const file = await fs.readFile(process.cwd() + '/src/data/evaluations.json', 'utf-8');
-  const evaluations: Avaliacao[] = JSON.parse(file);
-  const { title, description, grade } = await request.json();
-  
-  const index = evaluations.findIndex(e => e.id == params.id);
-  if (index !== -1) {
-    const updatedEvaluation = { ...evaluations[index], title, description, grade };
-    evaluations.splice(index, 1, updatedEvaluation);
-    await fs.writeFile(process.cwd() + '/src/data/evaluations.json', JSON.stringify(evaluations));
-    return NextResponse.json({ msg: "Avaliação atualizada com sucesso." });
-  } else {
-    return NextResponse.json({ msg: "Avaliação não encontrada" });
-  }
+    const file = await fs.readFile(process.cwd() + '/src/data/evaluations.json', 'utf-8');
+    const evaluations: Avaliacao[] = JSON.parse(file);
+    const { nome, nota, descricao } = await request.json();
+    const index = evaluations.findIndex(e => e.id === params.id);
+
+    if (index !== -1) {
+        const updatedEvaluation = { nome, nota, descricao, id: params.id } as Avaliacao;
+        evaluations.splice(index, 1, updatedEvaluation);
+        await fs.writeFile(process.cwd() + '/src/data/evaluations.json', JSON.stringify(evaluations));
+        return NextResponse.json({ msg: "Avaliação atualizada com sucesso." });
+    }
 }
 
-// Função DELETE - Excluir avaliação por ID
 export async function DELETE(request: Request, { params }: { params: { id: number } }) {
-  const file = await fs.readFile(process.cwd() + '/src/data/evaluations.json', 'utf-8');
-  const evaluations: Avaliacao[] = JSON.parse(file);
-  
-  const index = evaluations.findIndex(e => e.id == params.id);
-  if (index !== -1) {
-    evaluations.splice(index, 1);
-    await fs.writeFile(process.cwd() + '/src/data/evaluations.json', JSON.stringify(evaluations));
-    return NextResponse.json({ msg: "Avaliação excluída com sucesso." });
-  } else {
-    return NextResponse.json({ msg: "Avaliação não encontrada" });
-  }
+    const file = await fs.readFile(process.cwd() + '/src/data/evaluations.json', 'utf-8');
+    const evaluations: Avaliacao[] = JSON.parse(file);
+    const index = evaluations.findIndex(e => e.id === params.id);
+
+    if (index !== -1) {
+        evaluations.splice(index, 1);
+        await fs.writeFile(process.cwd() + '/src/data/evaluations.json', JSON.stringify(evaluations));
+        return NextResponse.json({ msg: "Avaliação excluída com sucesso." });
+    }
 }
