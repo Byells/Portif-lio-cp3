c9afc983f2afbc46cfc3f29a57c79d3eb93d6ea2
Byells <marreta.11.lima@gmail.com>
2024-10-27 21:12:41 -0300

    att about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,6 +5,9 @@ export default async function About() {
         <h1 className="text-4xl font-bold text-center mb-10 text-blue-600">
           Sobre
         </h1>
+        <p>
+          Esperamos um 10
+        </p>
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8"></div>
       </div>
     </div>
